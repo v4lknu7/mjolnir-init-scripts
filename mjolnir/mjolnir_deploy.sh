5a26a53be034e9fe69f2400a4f53fdba48939849
v4lknu7 <124394355+v4lknu7@users.noreply.github.com>
2023-09-01 21:02:03 +0100

    Integrate with doppler secrets management

--- mjolnir/mjolnir_deploy.sh
+++ mjolnir/mjolnir_deploy.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
 
-MJOLNIR_REPO="https://github.com/v4lknu7/mjolnir.git"
-
 MJOLNIR_HOME_ENVVAR_NAME="MJOLNIR_PATH"
 MJOLNIR_HOME_ENVVAR_VALUE="$HOME/mjolnir"
 
 PIP_HOME="$HOME/.local/bin"
 
 SHELL_FILE="$HOME/.bashrc"
-
-SLACK_INTEGRATION_ENV_FILE="$HOME/.slackenv"
-SLACK_INTEGRATION_APITOKEN_VAR_NAME="SLACK_API_TOKEN"
-SLACK_INTEGRATION_CHANID_FULLACTIVITY_VAR_NAME="SLACK_CHANNEL_ID_FULLACTIVITY"
-SLACK_INTEGRATION_CHANID_FINDINGSONLY_VAR_NAME="SLACK_CHANNEL_ID_FINDINGSONLY"
 
 SLEEPTIME=3
 
@@ -26,9 +19,6 @@ MJOLNIR_HOME_TOOLS_FOLDER="${MJOLNIR_HOME_ENVVAR_VALUE}/tools"
 ANSIBLE_CONF_FOLDER="${MJOLNIR_HOME_ENVVAR_VALUE}/ansible"
 ANSIBLE_INVENTORY_FILE="${ANSIBLE_CONF_FOLDER}/ansible.inventory"
 ANSIBLE_INVENTORY_OPENVAS_GROUPNAME="openvas"
-
-OPENVASPG_CONF_FOLDER="${MJOLNIR_HOME_ENVVAR_VALUE}/.pg"
-OPENVASPG_CONF_FILE="${OPENVASPG_CONF_FOLDER}/.openvaspg"
 
 printf "\n"
 echo "ODg4YiAgICAgZDg4OCAgZDhiICAgICAgICAgIDg4OCAgICAgICAgICBkOGIKODg4OGIgICBkODg4OCAgWThQICAgICAgICAgIDg4OCAgICAgICAgICBZOFAKODg4ODhiLmQ4ODg4OCAgICAgICAgICAgICAgIDg4OAo4ODhZODg4ODhQODg4IDg4ODggIC5kODhiLiAgODg4IDg4ODg4Yi4gIDg4OCA4ODhkODg4Cjg4OCBZODg4UCA4ODggIjg4OCBkODgiIjg4YiA4ODggODg4ICI4OGIgODg4IDg4OFAiCjg4OCAgWThQICA4ODggIDg4OCA4ODggIDg4OCA4ODggODg4ICA4ODggODg4IDg4OAo4ODggICAiICAgODg4ICA4ODggWTg4Li44OFAgODg4IDg4OCAgODg4IDg4OCA4ODgKODg4ICAgICAgIDg4OCAgODg4ICAiWTg4UCIgIDg4OCA4ODggIDg4OCA4ODggODg4CiAgICAgICAgICAgICAgIDg4OAogICAgICAgICAgICAgIGQ4OFAKICAgICAgICAgICAgODg4UCIKCigiQnV0IGZpbmVzdCBvZiB0aGVtIGFsbCwgJ1RoZSBDcnVzaGVyJyBpdCBpcyBjYWxsZWQ6IE1qw7ZsbmlyISBIYW1tZXIgb2YgVGhvciEiKQ==" | base64 -d
@@ -59,9 +49,11 @@ fi
 printf "${GREEN}\n\n\n*****************************\n"
 printf "cloning and configuring mjolnir\n"
 printf "*****************************\n\n${NC}"
+git_user=$(doppler secrets get DOPPSECRET_GITHUB_CREDENTIALS --plain | jq -r ".user")
+git_pat=$(doppler secrets get DOPPSECRET_GITHUB_CREDENTIALS --plain | jq -r ".token")
 rm -rf ${MJOLNIR_HOME_ENVVAR_VALUE}
 printf "${GREEN}\n\nCloning repo...${NC}\n"
-git clone ${MJOLNIR_REPO} ${MJOLNIR_HOME_ENVVAR_VALUE}
+git clone https://${git_pat}@github.com/${git_user}/mjolnir.git ${MJOLNIR_HOME_ENVVAR_VALUE}
 printf "${GREEN}\n\nchmoding executable files...${NC}\n"
 find ${MJOLNIR_HOME_ENVVAR_VALUE} -name "*.sh" -exec chmod 700 {} \;
 printf "${GREEN}\n\nSetting ${MJOLNIR_HOME_ENVVAR_NAME} env variable...${NC}\n"
@@ -71,14 +63,7 @@ else
   echo "export ${MJOLNIR_HOME_ENVVAR_NAME}=${MJOLNIR_HOME_ENVVAR_VALUE}" >> "${SHELL_FILE}"
   printf "${GREEN}${MJOLNIR_HOME_ENVVAR_NAME} was set${NC}\n"
 fi
-printf "${GREEN}\n\nConfiguring integration with Slack...${NC}\n"
-read -p "Slack Bot User OAuth Token: " slack_token
-read -p "Slack Channel ID (for sending full activity notifications): " slack_chid_fullactivity
-read -p "Slack Channel ID (for sending found vulnerabilities and findings only): " slack_chid_findingsonly
-printf "%s\n"\
-  "export ${SLACK_INTEGRATION_APITOKEN_VAR_NAME}=${slack_token}"\
-  "export ${SLACK_INTEGRATION_CHANID_FULLACTIVITY_VAR_NAME}=${slack_chid_fullactivity}"\
-  "export ${SLACK_INTEGRATION_CHANID_FINDINGSONLY_VAR_NAME}=${slack_chid_findingsonly}" > "${SLACK_INTEGRATION_ENV_FILE}"
+
 printf "${GREEN}\n\n\nDone. mjolnir configured.\n${NC}"
 sleep $SLEEPTIME
 
@@ -98,7 +83,7 @@ if grep "${USER}.*nmap" /etc/sudoers; then
   printf "${GREEN}passwordless sudo 'nmap -sU --top-ports' already set${NC}\n"
 else
   echo "${USER} ALL=(ALL) NOPASSWD: /usr/bin/nmap -sU --top-ports *" | sudo tee -a /etc/sudoers
-  printf "${GREEN}Passwordless 'nmap -sU --top-ports' was set${NC}\n"
+  printf "${GREEN}passwordless 'nmap -sU --top-ports' was set${NC}\n"
 fi
 
 mkdir -p ${MJOLNIR_HOME_TOOLS_FOLDER}
@@ -126,7 +111,9 @@ sleep $SLEEPTIME
 printf "${GREEN}\n\n\n*****************************\n"
 printf "configuring censys\n"
 printf "*****************************\n\n${NC}"
-censys config
+censys_id=$(doppler secrets get DOPPSECRET_CENSYS_CREDENTIALS --plain | jq -r ".id")
+censys_apikey=$(doppler secrets get DOPPSECRET_CENSYS_CREDENTIALS --plain | jq -r ".token")
+echo -e "${censys_id}\n${censys_apikey}\nn" | censys config
 printf "${GREEN}\nDone. censys configured.\n${NC}"
 
 printf "${GREEN}\n\n\n*****************************\n"
@@ -165,27 +152,6 @@ ansible_ssh_common_args="-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/
 ansible_ssh_private_key_file="${key_file}"
 EOF
 printf "${GREEN}\nansible inventory created at ${ANSIBLE_INVENTORY_FILE}:\n${NC}"
-
-read -p "(OpenVAS scan results storage) PostgreSQL DB host: " openvaspg_host
-read -p "(OpenVAS scan results storage) PostgreSQL DB port: " openvaspg_port
-read -p "(OpenVAS scan results storage) PostgreSQL DB name: " openvaspg_name
-read -p "(OpenVAS scan results storage) PostgreSQL DB table name: " openvaspg_table
-read -p "(OpenVAS scan results storage) PostgreSQL DB user: " openvaspg_user
-read -s -p "(OpenVAS scan results storage) PostgreSQL DB user pass: " openvaspg_pass
-echo
-read -p "(OpenVAS scan results storage) PostgreSQL DB root certificate file name (for TLS connection) - SAVE IT IN ${OPENVASPG_CONF_FOLDER}: " openvaspg_cert
-mkdir -p ${OPENVASPG_CONF_FOLDER}
-cat <<EOF > ${OPENVASPG_CONF_FILE}
-[db]
-openvaspghost=${openvaspg_host}
-openvaspgport=${openvaspg_port}
-openvaspgdb=${openvaspg_name}
-openvaspgtable=${openvaspg_table}
-openvaspguser=${openvaspg_user}
-openvaspgpass=${openvaspg_pass}
-openvaspgrootcertfilename=${openvaspg_cert}
-EOF
-printf "${GREEN}\nPostgreSQL OpenVAS DB conf file created at ${OPENVASPG_CONF_FILE}:\n${NC}"
 
 printf "${GREEN}\nDone. Environment configured.\n${NC}"
 sleep $SLEEPTIME
